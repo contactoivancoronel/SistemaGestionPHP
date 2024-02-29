01e9219185fda4f3135d53129a99eb5b7c589a45
contactoivancoronel <contactoivancoronel@gmail.com>
2024-02-29 14:28:19 -0300

    Modificar credenciales

--- php/enviar_mail.php
+++ php/enviar_mail.php
@@ -15,12 +15,12 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;        //SMTP:: DEBUG_OFF             //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->Host       = '';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'heladeriaunlz@gmail.com';                     //SMTP username
-    $mail->Password   = 'heladeria12345';                               //SMTP password
+    $mail->Username   = '';                     //SMTP username
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Port       = ;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->setFrom('heladeriaunlz@gmail.com', 'FLAMENCA ALISADOS');
